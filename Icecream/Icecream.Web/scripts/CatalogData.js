588c9a985b9a6f7948b517dde6c59a3f8bfd9f01
kirama <kirama.aliyandadu@gmail.com>
2017-08-11 14:23:19 -0700

    VERSIÓN ACTUALIZADA

---
+++ Icecream/Icecream.Web/scripts/CatalogData.js
@@ -0,0 +1,45 @@
+﻿//jSON Object
+var MyCatalogData = {
+    "Ice1":
+        {
+            "Model": "IC001",
+            "Price $": 5.00,
+            "Description": "Ice cream taste chocolate.",
+            "imgUrl": "Images/Boxicecreamchocolate.jpg"
+        },
+    "Ice2":
+        {
+            "Model": "IC002",
+            "Price $": 5.00,
+            "Description": "Ice cream taste vainilla.",
+            "imgUrl": "Images/Boxvanilla.jpg"
+        },
+    "Ice3":
+        {
+            "Model": "IC003",
+            "Price $": 5.00,
+            "Description": "Ice cream taste strawberry.",
+            "imgUrl": "Images/strawberry.jpg"
+        },
+    "Ice4":
+        {
+            "Model": "IC004",
+            "Price $": 5.00,
+            "Description": "Ice cream taste caramel.",
+            "imgUrl": "Images/caramel.jpg"
+        },
+    "Ice5":
+        {
+            "Model": "IC005",
+            "Price $": 5.00,
+            "Description": "Ice cream taste kiwi.",
+            "imgUrl": "Images/kiwi.jpg"
+        },
+    "Ice6":
+        {
+            "Model": "IC006",
+            "Price $": 5.00,
+            "Description": "Ice cream taste condensed milk.",
+            "imgUrl": "Images/milk.jpg"
+        },
+}

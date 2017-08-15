3df4be33d0d1dad73d918dea1a3714d96f38a0fb
kirama <kirama.aliyandadu@gmail.com>
2017-08-15 14:25:47 -0700

    Ejercicios del 15 de agosto del 2017

---
+++ Icecream 15 de agosto del 2017/Icecream.Web/scripts/Catalog.js
@@ -0,0 +1,19 @@
+﻿//Create a JSON Object that contains my products information.
+
+//Ready: After HTML loads
+$(document).ready(function () {
+    $("#DynamicHTML").append(buildMyProduct(MyCatalogData));
+});
+
+
+//Dynamic build your collection
+function buildMyProduct(obj){
+    var html="";
+    $.each(obj,function(element, object){
+            //html += "<div class='Catalog ProducItem'>"
+            html += "<div><a href='Detail.html?id=" + object.id + "'>" + object.title + "</a></div>";
+            html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
+            html += "<div>" + object.Description + "</div></div>";
+    });
+    return html;
+}
---
+++ Icecream 15 de agosto del 2017/Icecream.Web/scripts/CatalogData.js
@@ -0,0 +1,88 @@
+﻿//jSON Object
+var MyCatalogData = {
+    "Ice1":
+        {
+            "id": 0,
+            "title":"Chocolate",
+            "Model": "IC001",
+            "Price": "5.00",
+            "Description": "Ice cream taste chocolate.",
+            "imgUrl": "Images/chocolate.jpg",
+            "img": ["Images/chocolate.jpg", "Images/chocolate.jpg", "Images/chocolate.jpg"]
+            //"productImg":{
+            //    "img1":"Images/chocolate.jpg",
+            //    "img2": "Images/chocolate.jpg",
+            //    "img3": "Images/chocolate.jpg"
+            //}
+        },
+    "Ice2":
+        {
+            "id": 1,
+            "title": "Vainilla",
+            "Model": "IC002",
+            "Price": "5.00",
+            "Description": "Ice cream taste vainilla.",
+            "imgUrl": "Images/Boxvanilla.jpg",
+             "productImg":{
+                "img1":"Images/Boxvanilla.jpg",
+                "img2": "Images/Boxvanilla.jpg",
+                "img3": "Images/Boxvanilla.jpg"
+            }
+        },
+    "Ice3":
+        {
+            "id": 2,
+            "title": "Strawberry",
+            "Model": "IC003",
+            "Price": "5.00",
+            "Description": "Ice cream taste strawberry.",
+            "imgUrl": "Images/DaduStrawberry.jpg",
+            "productImg": {
+                "img1": "Images/DaduStrawberry.jpg",
+                "img2": "Images/DaduStrawberry.jpg",
+                "img3": "Images/DaduStrawberry.jpg"
+            }
+        },
+    "Ice4":
+        {
+            "id": 3,
+            "title": "Caramel",
+            "Model": "IC004",
+            "Price": "5.00",
+            "Description": "Ice cream taste caramel.",
+            "imgUrl": "Images/caramel.jpg",
+            "productImg": {
+                "img1": "Images/caramel.jpg",
+                "img2": "Images/caramel.jpg",
+                "img3": "Images/caramel.jpg"
+            }
+        },
+    "Ice5":
+        {
+            "id": 4,
+            "title": "Kiwi",
+            "Model": "IC005",
+            "Price": "5.00",
+            "Description": "Ice cream taste kiwi.",
+            "imgUrl": "Images/kiwi.jpg",
+            "productImg": {
+                "img1": "Images/kiwi.jpg",
+                "img2": "Images/kiwi.jpg",
+                "img3": "Images/kiwi.jpg"
+            }
+        },
+    "Ice6":
+        {
+            "id": 5,
+            "title": "Condensed milk",
+            "Model": "IC006",
+            "Price": "5.00",
+            "Description": "Ice cream taste condensed milk.",
+            "imgUrl": "Images/milk.jpg",
+            "productImg": {
+                "img1": "Images/milk.jpg",
+                "img2": "Images/milk.jpg",
+                "img3": "Images/milk.jpg"
+            }
+        },
+}
